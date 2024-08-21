42a6fcf5032bf6b1cc73d60cc1654885296405c9
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-08-21 01:38:24 -0300

    separate in updatable and drawable objects

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    y = SCREEN_HEIGHT / 2
-    x = SCREEN_WIDTH / 2
-    p1 = Player(x, y)
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -19,8 +24,10 @@ def main():
                 return
 
         pygame.Surface.fill(screen, (0, 0, 0))
-        p1.update(dt)
-        p1.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         t = clock.tick(60)
         dt = t/1000
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -10,7 +10,7 @@ class Player(CircleShape):
         self.rotation = 0
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "red", self.triangle(), 2)
 
     # in the player class
     def triangle(self):
@@ -22,10 +22,19 @@ class Player(CircleShape):
         return [a, b, c]
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0,1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.rotate(0-dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
